49e9a049f4ea31ccea3d70fee35216be354423a7
AH-dark <ahdark@outlook.com>
2023-11-08 18:28:20 +0800

    refactor: use as monorepo package

---
+++ web/en-US/index.ts
@@ -0,0 +1,11 @@
+import common from "./common.json";
+import language from "./language.json";
+import notify from "./notify.json";
+
+const enUs = {
+  common,
+  language,
+  notify,
+};
+
+export default enUs;
---
+++ web/zh-CN/index.ts
@@ -0,0 +1,9 @@
+import common from "./common.json";
+import notify from "./notify.json";
+
+const zhCn = {
+  common,
+  notify,
+};
+
+export default zhCn;
